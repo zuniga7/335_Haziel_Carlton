9f904082bad5ea7a392caeefba6cf5cb1da25a50
zuniga7 <zuniga7@email.arizona.edu>
2013-01-31 13:02:37 -0700

    -del comments

--- 335-JukeBox/src/Song.java
+++ 335-JukeBox/src/Song.java
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
 
-
-
-// the best comment ever
-// oisudfoiu
-=======
-// carltons comment
->>>>>>> branch 'master' of https://github.com/zuniga7/335_Haziel_Carlton.git
-// HAziel's comment
-	
-	
-	
-	// haziel 2 -- sdfsdf
 public class Song {
 
 }
