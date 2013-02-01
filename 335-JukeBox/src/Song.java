248118bfaae6ddbfd755511a1352fad6a3bd2c6f
zuniga7 <zuniga7@email.arizona.edu>
2013-02-01 13:42:00 -0700

    - start of song class

--- 335-JukeBox/src/Song.java
+++ 335-JukeBox/src/Song.java
@@ -1,4 +1,51 @@
-
 public class Song {
+	private String songName;
+	private int numPlays = 0;
+	private int songLength;
+
+	/**
+	 * constructor for a song object
+	 */
+	public Song(String name, int length) {
+		songName = name;
+		songLength = length; // length of song is in seconds
+	}
+
+	/**
+	 * gets the length of a song in seconds
+	 * 
+	 * @return songLength
+	 */
+	public int getLength() {
+		return songLength;
+	}
+
+	/**
+	 * gets the name of a song
+	 * 
+	 * @return songName
+	 */
+	public String getName() {
+		return songName;
+
+	}
+
+	/**
+	 * gets the the total number of plays this song has been played in a single
+	 * day
+	 * 
+	 * @return numPlays
+	 */
+	public int getNumPlays() {
+		return numPlays;
+
+	}
+
+	/**
+	 *  does the necessary steps in order to play a song
+	 */
+	public void playSong() {
+
+	}
 
 }
