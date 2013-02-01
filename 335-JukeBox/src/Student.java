02a8eb900b2be6210bf8a824c9703525d1358308
carltonochoa <carltonochoa@10.135.197.138>
2013-02-01 13:32:25 -0700

    - StudentList

--- 335-JukeBox/src/Student.java
+++ 335-JukeBox/src/Student.java
@@ -1,4 +1,39 @@
 
+
+
 public class Student {
+	
+	private int startingTime = 1500 * 60;
+	private int songChosenLength;
+	private String studentName = "";
+	private int IDNumber;
+	private StudentList theStudents = new StudentList();
+	
+	
+	public Student(String name, int identification) {
+		
+		studentName = name;
+		IDNumber = identification;
+		
+	}
+	
+	public void chooseSong(){
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
