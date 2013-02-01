1f80378d11b6764d694222dff2a38dc13186f676
zuniga7 <zuniga7@email.arizona.edu>
2013-02-01 14:01:25 -0700

    -song collection start

--- 335-JukeBox/src/SongCollection.java
+++ 335-JukeBox/src/SongCollection.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class SongCollection {
+	private ArrayList<Song> songList;
+	private Queue<Song> playList;
+
+	/**
+	 * constructor to create a new Song Collection of song objects
+	 */
+	public SongCollection() {
+		songList = new ArrayList<Song>();
+		playList = new LinkedList<Song>();
+	}
+
+	/**
+	 * adds a song object into the song collection (ArrayList songList)
+	 */
+	public void addToSongList(Song newSong) {
+		songList.add(newSong);
+	}
+
+	/**
+	 * adds a song object into the playlist queue (Queue playList)
+	 */
+	public void addToPlayList(Song selectedSong) {
+		playList.add(selectedSong);
+	}
 
 }
