0940995baed3809e4f5185f22d9590022cf3d3ec
carltonochoa <carltonochoa@10.135.197.138>
2013-02-01 14:23:33 -0700

    -  new update

--- 335-JukeBox/src/Student.java
+++ 335-JukeBox/src/Student.java
@@ -1,42 +1,69 @@
-
-
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Student {
-	
+
 	private int startingTime = 1500 * 60;
 	private int songChosenLength;
+	private int currentTimeRemaining;
 	private String studentName = "";
 	private int IDNumber;
 	private StudentList theStudents = new StudentList();
 	private SongCollection songCollection = new SongCollection();
-	
-	
+	private int playsForTheDay = 0;
+	private Calendar cal1 = new GregorianCalendar();
+	private boolean canPlay;
+
 	public Student(String name, int identification) {
-		
+
 		studentName = name;
 		IDNumber = identification;
-		
+		currentTimeRemaining = startingTime;
+
 	}
-	
+
 	public void chooseSong(Song nameOfSong){
 		
-		for (int i = 0; i <)
+		songChosenLength = nameOfSong.getLength();
+		this.subtractTime(songChosenLength);
+		if(st)
+
+			
+			
+		}
+
+	public int getAvailableMinutes() {
+
+		return currentTimeRemaining;
+
+	}
+
+	public void calculateCurrentTimeRemaining() {
+
+//		int temp = this.getAvailableMinutes();
+
+	}
+
+	public void subtractTime(int songLength) {
+		
+		currentTimeRemaining = currentTimeRemaining - songLength;
 		
 		
 		
+
+	}
+	
+	public boolean studentCanPlay(){
 		
-		
+		if (playsForTheDay > 2){
+			return false;
+			
+		}
+		else{
+			return true;
+		}
 		
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
