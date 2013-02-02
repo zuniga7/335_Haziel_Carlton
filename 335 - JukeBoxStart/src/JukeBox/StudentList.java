b8ea59ca3e5a43aec81683b964fb9ef27f94d898
carltonochoa <carltonochoa@192.168.1.4>
2013-02-02 08:49:43 -0700

    - new method for StudentList

--- 335 - JukeBoxStart/src/JukeBox/StudentList.java
+++ 335 - JukeBoxStart/src/JukeBox/StudentList.java
@@ -4,23 +4,37 @@ import java.util.HashMap;
 
 //hardcore those into the collection
 public class StudentList<K, V> {
-	
-	
-	private HashMap<String,Integer> students = new HashMap<String, Integer>();
-	
+
+	private HashMap<String, Integer> students = new HashMap<String, Integer>();
+
+	/**
+	 * Hashmap of current users and their passwords.
+	 */
 	public StudentList() {
-		
-		
+
 		students.put("Ali", 1111);
 		students.put("Chris", 2222);
 		students.put("River", 3333);
 		students.put("Ryan", 4444);
-		
-		
-		
-		
-		
-		
+
+	}
+
+	/**
+	 * Determines whether the user exists and whether the password matches
+	 * 
+	 * @param name
+	 * @param password
+	 * @return boolean of whether the user exists
+	 */
+
+	public boolean canUserLogIn(String name, int password) {
+
+		if (students.get(name) == password) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 }
